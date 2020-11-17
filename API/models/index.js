a39ffabcd1beeae10a84ba033975e036bc9d0759
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-17 15:33:53 +0900

    feat: 모델 스키마 연결

--- API/models/index.js
+++ API/models/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 import Sequelize from 'sequelize';
+import Comment from './Comment';
+import Movie from './Movie';
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')['default'][env];
-const db = {};
+
+const db = {};  
 const sequelize = new Sequelize(
   config.database,
   config.username,
@@ -10,6 +13,12 @@ const sequelize = new Sequelize(
   config,
 );
 
+db.Movie = Movie;
+db.Comment = Comment;
+
+Object.keys(db).forEach(modelName => {
+  db[modelName].init(sequelize);
+});
 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
