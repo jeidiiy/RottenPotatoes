68577c35370d78aafc870ecdbc09720af624ba8e
galaxy <galaxyhi4276@gmail.com>
2020-11-20 04:39:57 +0900

    feat: Store에 Saga 연동

--- App/store/index.js
+++ App/store/index.js
@@ -1,11 +1,16 @@
 import reducers from '../reducer';
 import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../saga';
 
 export default function initStore() {
+  const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
     reducers,
+    applyMiddleware(sagaMiddleware),
   );
-
+  sagaMiddleware.run(rootSaga);
+  
   return store;
 }
 
