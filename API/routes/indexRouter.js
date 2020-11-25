c82267e04b40e4fdb9fff2c3eb4f0c7f54bed99f
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-25 17:51:15 +0900

    feat: 백엔드 movie 정보 저장 및 Comment 취합 전송 API

--- API/routes/indexRouter.js
+++ API/routes/indexRouter.js
@@ -1,7 +1,22 @@
+import sequelize from '../models';
+const { Comment, Movie } = sequelize;
+
 const indexRouter = require('express').Router();
+
 
 indexRouter.get('/', (req, res) => {
   res.send('ok');
+});
+
+indexRouter.get('/movie/:id', (req, res, next) => {
+  const movie = await Movie.findOrCreate({
+    where: { movieId: req.params.id },
+    include: [{
+      model: Comment,
+    }],
+  });
+
+  return res.json(movie);
 });
 
 
