5f82e116ebd5eb8c0d2dbd1ef7b71248bece50ec
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-25 17:18:13 +0900

    feat: 영화 단일 정보 불러오는 액션과 saga 작성

--- App/reducer/movie.js
+++ App/reducer/movie.js
@@ -4,11 +4,20 @@ const initialState = {
   loading: false,
   moreLoading: false,
   movies: [],
+  movieInfo: {},
   movieId: 1,
   page: 1,
   getMovieLoading: false,
   getMovieDone: false,
   getMovieError: null,
+
+  getMovieInfoLoading: false,
+  getMovieInfoDone: false,
+  getMovieInfoError: null,
+
+  commentLoading: false,
+  commentSuccess: false,
+  commentError: null,
 }
 
 export const GET_MOVIE_REQUEST = 'GET_MOVIE_REQUEST';
@@ -18,6 +27,14 @@ export const GET_MOVIE_FAILURE = 'GET_MOVIE_FAILURE';
 export const GET_MOVIE_MORE_REQUEST = 'GET_MOVIE_MORE_REQUEST';
 export const GET_MOVIE_MORE_SUCCESS = 'GET_MOVIE_MORE_SUCCESS';
 export const GET_MOVIE_MORE_FAILURE = 'GET_MOVIE_MORE_FAILURE';
+
+export const GET_MOVIE_INFO_REQUEST = 'GET_MOVIE_INFO_REQUEST';
+export const GET_MOVIE_INFO_SUCCESS = 'GET_MOVIE_INFO_SUCCESS';
+export const GET_MOVIE_INFO_FAILURE = 'GET_MOVIE_INFO_FAILURE';
+
+export const WRITE_COMMENT_REQUEST = 'WRITE_COMMENT_REQUEST';
+export const WRITE_COMMENT_SUCCESS = 'WRITE_COMMENT_SUCCESS';
+export const WRITE_COMMENT_FAILURE = 'WRITE_COMMENT_FAILURE';
 
 const movieReducer = handleActions(
   {
@@ -76,6 +93,31 @@ const movieReducer = handleActions(
         getMovieError,
         getMovieDone: false,
       };
+    },
+    [GET_MOVIE_INFO_REQUEST]: state => {
+      return {
+        ...state,
+        getMovieInfoLoading: true,
+        getMovieInfoError: null,
+        getMovieInfoDone: false,
+      }
+    },
+    [GET_MOVIE_INFO_SUCCESS]: (state, { movie }) => {
+      return {
+        ...state,
+        getMovieInfoLoading: false,
+        getMovieInfoError: null,
+        getMovieInfoDone: true,
+        movieInfo: movie,
+      }
+    },
+    [GET_MOVIE_INFO_FAILURE]: (state, dispatch) => {
+      return {
+        ...state,
+        getMovieInfoLoading: false,
+        getMovieInfoError: null,
+        getMovieInfoDone: false,
+      }
     },
   },
   initialState
