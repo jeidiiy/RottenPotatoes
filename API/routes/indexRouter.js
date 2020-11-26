6c1903edaedd2b80321bb7e2b5726e225216c7db
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-27 08:13:19 +0900

    feat: Async/Await IndexRouter 오타 수정

--- API/routes/indexRouter.js
+++ API/routes/indexRouter.js
@@ -8,7 +8,7 @@ indexRouter.get('/', (req, res) => {
   res.send('ok');
 });
 
-indexRouter.get('/movie/:id', (req, res, next) => {
+indexRouter.get('/movie/:id', async (req, res, next) => {
   const movie = await Movie.findOrCreate({
     where: { movieId: req.params.id },
     include: [{
