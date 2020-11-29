8ae1363d499d71b319dd28cef6cfff766416c388
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-29 23:35:41 +0900

    feat: 서버단 댓글 정보 전송

--- API/routes/indexRouter.js
+++ API/routes/indexRouter.js
@@ -16,5 +16,19 @@ indexRouter.get('/movie/:id', async (req, res) => {
   return res.status(200).send('ok');
 });
 
+indexRouter.get('/comments/:id', async (req, res, next) => {
+  try {
+    const comments = await Comment.findAll({
+      where: { MovieMovieId: req.params.id }
+    });
+
+    console.log(comments);
+    return res.status(203).json(comments);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  };
+}); 
+
 
 export default indexRouter;
